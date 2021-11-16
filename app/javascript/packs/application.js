952f562a7759cbd2948eba2f7b878adaab48c3eb
jonathan kerbrat <jonathan.kerbrat@hotmail.com>
2021-11-16 16:02:47 +0100

    mep bill v1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,34 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import initName from "../plugins/bill"
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-export { initName}
+import initCart from "../plugins/bill";
+
+document.addEventListener('turbolinks:load', () => {
+  initCart();
+});
+
+// const temp = {}
+//   const list_articles = document.getElementById("article")
+//   const cart = {
+//     names: [],
+//     prices: []
+//   }
+
+// document.querySelectorAll('button').forEach(item => {
+  //   item.addEventListener('click', event => {
+//     const name = event.target.dataset.name;
+//     const price = parseFloat(event.target.dataset.price);
+//     cart.names.push(name), cart.prices.push(price)
+//     temp[`${name}`] = price
+//     cart.names.forEach(name => list_articles.insertAdjacentHTML("beforeend", `<tr><td >${name}</td><td >${temp[name]}</td></tr></tr`));
+//   })
+
+// })
+// cart.names.forEach(name =>
+//   list_articles.insertAdjacentHTML("beforeend", `<tr><td >${name}</td><td >${temp[name]}</td></tr></tr`));
--- app/javascript/plugins/bill.js
+++ app/javascript/plugins/bill.js
@@ -1,7 +1,20 @@
-// Recuperation data (id , name , price)
 
+let temp = {}
+const initCart = () => {
+  let listArticles = document.getElementById("article")
+  if (!listArticles)
+  return
+  const cart = document.querySelectorAll('.card-product-infos .add-to-cart').forEach(item => {
+    item.addEventListener('click', event => {
+    let name = event.currentTarget.dataset.name;
+    let price = parseFloat(event.currentTarget.dataset.price);
+    temp[`${name}`]= price
+    const key = Object.keys(temp)
+      const last = key[key.length - 1]
+      listArticles.insertAdjacentHTML("beforeend", `<tr><td >${last}</td><td >${temp[last]}</td></tr></tr`);
+  })
+})
+Object.keys(temp).forEach(name => listArticles.insertAdjacentHTML("beforeend", `<tr><td >${name}</td><td >${temp[name]}</td></tr></tr`));
+}
 
-
-// export { initName};
-
-// ------------------
+export default initCart
