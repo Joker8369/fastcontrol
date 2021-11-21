9b9a5212b0267b8d34b7c0da27c8173fb155f772
jonathan kerbrat <jonathan.kerbrat@hotmail.com>
2021-11-21 20:16:50 +0100

    maj total provisional cart

--- app/javascript/plugins/bill.js
+++ app/javascript/plugins/bill.js
@@ -11,9 +11,7 @@ const initCart = () => {
       let price = parseFloat(event.currentTarget.dataset.price);
       let q = document.getElementById(`${name}`)
       let subsum = document.getElementById(`int${name}`)
-      let intTotal = document.getElementsByClassName(`totalCol`)
       const key = Object.keys(bill)
-      const last = key[key.length - 1]
       if (key.includes(`${name}`)){
         const qty = prompt("quantité");
         bill[`${name}`] = [price, qty]
@@ -25,6 +23,20 @@ const initCart = () => {
         bill[`${name}`] = [price, qty]
         listArticles.insertAdjacentHTML("beforeend", `<tr><td >${name}</td><td >${bill[`${name}`][0]}</td><td id=${name}>${bill[`${name}`][1]}</td><td id = int${name} class="totalCol">${bill[`${name}`][1] * bill[`${name}`][0]}</td></tr`)
       };
+      let subtotal = []
+      Object.keys(bill).forEach(name => {
+        let sum = bill[`${name}`][1] * bill[`${name}`][0]
+        subtotal.push(sum)
+      });
+      let result = 0
+      const total = document.querySelector(".total")
+      if ((subtotal.length === 0)) {
+        return total.innerText = result
+      } else {
+        const reducer = (previousValue, currentValue) => previousValue + currentValue;
+        let result = subtotal.reduce(reducer)
+        total.innerText = result
+      }
     });
   });
   let subtotal = []
@@ -33,15 +45,11 @@ const initCart = () => {
     listArticles.insertAdjacentHTML("beforeend", `<tr><td >${name}</td><td >${bill[`${name}`][0]}</td><td id=${name}>${bill[`${name}`][1]}</td><td id = int${name} class="totalCol">${sum}</td></tr`)
     subtotal.push(sum)
   });
-  console.log(subtotal)
   let result = 0
   const total = document.querySelector(".total")
-  console.log (total)
   if ((subtotal.length === 0)){
-    console.log('la')
     return total.innerText = result
     } else {
-    console.log("je suis la ", subtotal)
     const reducer = (previousValue, currentValue) => previousValue + currentValue;
     let result = subtotal.reduce(reducer)
     total.innerText = result
